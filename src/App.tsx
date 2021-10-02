eecb2104ee95d4d41bc04c8400e5b33decb049d2
estePecora <b.estefaniapecora@gmail.com>
2021-10-02 13:53:27 -0300

    added/json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+
+  useEffect(() => {
+    console.log('hello world')
+  }, [])
+
+  
   return (
     <div className="App">
       <h1>MOVIE DATA</h1>
